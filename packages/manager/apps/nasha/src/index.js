9b6781703edf7e7a90611b493c783a79682d1f2b
Marie JONES <marie.jones@corp.ovh.com>
2020-08-03 10:14:34 +0200

    feat(nasha): redirect to default route on application start
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/nasha/src/index.js
+++ packages/manager/apps/nasha/src/index.js
@@ -12,4 +12,8 @@ import './index.scss';
 
 Environment.setRegion(__WEBPACK_REGION__);
 
-angular.module('nashaApp', [ovhManagerNasha]);
+angular.module('nashaApp', [ovhManagerNasha]).config(
+  /* @ngInject */ ($urlRouterProvider) => {
+    $urlRouterProvider.otherwise('/paas/nasha');
+  },
+);
